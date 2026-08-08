fa95064a4d36a95472f84de5fe591fe7b4043c39
utsubo256 <96920121+utsubo256@users.noreply.github.com>
2026-08-08 17:15:57 +0900

    ブログの編集、更新、削除を所有者のみに制限

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,13 +3,15 @@
 class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
-  before_action :set_blog, only: %i[show edit update destroy]
+  before_action :set_blog, only: %i[edit update destroy]
 
   def index
     @blogs = Blog.search(params[:term]).published.default_order
   end
 
-  def show; end
+  def show
+    @blog = Blog.find(params[:id])
+  end
 
   def new
     @blog = Blog.new
@@ -44,7 +46,7 @@ class BlogsController < ApplicationController
   private
 
   def set_blog
-    @blog = Blog.find(params[:id])
+    @blog = current_user.blogs.find(params[:id])
   end
 
   def blog_params
